aeb8cee8c9115752696785bbc30d7a4d58d71995
AlanNJ <alannpn75@gmail.com>
2021-07-07 19:36:40 +0545

    first-commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
- 		****This is my portfolio website****
+ 		     ****This is my portfolio website****
 
-      Use the source code to get the idea how things are going on it.
-      But dont copy paste everythings it will detoriate you more as a programmer.
+    Use the source code to get the idea how things are going on it and use for your maximum 	output.
+    But dont copy paste everythings it will detoriate you more as a programmer.
 
 		THANK YOU !! Stay Safe !! Stay Connected !!
 			   **THE ALAN'S HUB**
--- js/app.js
+++ js/app.js
@@ -1,113 +1,99 @@
-TweenMax.to(".land", 4, {
-    delay: 1,
-    top: "-110%",
-    ease: Expo.easeInOut
-})
-
-TweenMax.from("h1", 3, {
-    delay: 2,
-    opacity: 0,
-    y: 20,
-    ease: Expo.easeInOut
+TweenMax.to(".land", 3, {
+  delay: 0,
+  top: "-110%",
+  ease: Expo.easeInOut,
 });
 
-TweenMax.from(".text h2", 2,{
-    delay:3,
-    opacity:0,
-    x:50,
-    ease: Expo.easeInOut
+TweenMax.from("h1", 2, {
+  delay: 2,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut,
+});
 
+TweenMax.from(".text h2", 2, {
+  delay: 3,
+  opacity: 0,
+  x: 50,
+  ease: Expo.easeInOut,
+});
 
-})
+TweenMax.from(".text p", 3, {
+  delay: 4,
+  opacity: 0,
+  x: 100,
+  ease: Expo.easeOut,
+});
+TweenMax.from(".nav h2", 3, {
+  delay: 3,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut,
+});
 
-TweenMax.from(".text p",3,{
-    delay:4,
-    opacity:0,
-    x:100,
-    ease:Expo.easeOut
+TweenMax.from(".nav ion-icon", 3, {
+  delay: 3,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut,
+});
 
-})
-TweenMax.from(".nav h2", 3,{
-    delay:3,
-    opacity:0,
-    y:20,
-    ease: Expo.easeInOut
-})
+TweenMax.from(".button", 2, {
+  delay: 4.4,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut,
+});
+TweenMax.from(".media  ul li", 4, {
+  delay: 4,
+  opacity: 0,
+  y: 40,
+  ease: "bounce.out",
+});
 
-TweenMax.from(".nav ion-icon", 3,{
-    delay:3,
-    opacity:0,
-    y:20,
-    ease: Expo.easeInOut
-})
-
-TweenMax.from(".button", 2,{
-    delay:4.4,
-    opacity:0,
-    y:20,
-    ease: Expo.easeInOut
-})
-TweenMax.from(".media  ul li",4,{
-    delay:4,
-    opacity:0,
-    y:40,
-    ease: "bounce.out"
-})
-
-TweenMax.from(".copyright", 5,{
-    delay:4.4,
-    opacity:0,
-    y:20,
-    ease: Expo.easeInOut
-})
+TweenMax.from(".copyright", 5, {
+  delay: 4.4,
+  opacity: 0,
+  y: 20,
+  ease: Expo.easeInOut,
+});
 
 TweenMax.from(".event", 4, {
-    delay:5,
-    opacity:0,
-    x:20,
-    ease:Expo.easeOut
-})
+  delay: 5,
+  opacity: 0,
+  x: 20,
+  ease: Expo.easeOut,
+});
 
-const learnMore = function(e){
-    document.querySelector('.event').style.display = 'block';
-    document.querySelector("#first").style.display = "none";
-    
-    window.scrollBy(0,828);
-    document.body.style.background = "black";
+const learnMore = function (e) {
+  document.querySelector(".event").style.display = "block";
+  document.querySelector("#first").style.display = "none";
+
+  window.scrollBy(0, 828);
+  document.body.style.background = "black";
+  e.preventDefault();
+};
+
+const Undo = function (e) {
+  if (e.target.classList.contains("del")) {
+    e.target.parentElement.parentElement.style.display = "none";
+    document.querySelector("#first").style.display = "inline-block";
+    window.onscroll = function () {
+      console.log(window.pageYOffset);
+    };
     e.preventDefault();
-    
-    
-    
-}
+  }
+};
 
-const Undo = function(e){
-    if(e.target.classList.contains('del')){
-        e.target.parentElement.parentElement.style.display = 'none';
-        document.querySelector('#first').style.display = 'inline-block'
-        window.onscroll = function(){
-            console.log(window.pageYOffset)    
-        }
-        e.preventDefault()
-        
-    }
-}
+const Premium = function () {
+  document.querySelector(".notification").style.display = "flex";
+  window.scrollBy(0, 280);
+  document.querySelector("#second").style.background =
+    "linear-gradient(135deg, rgb(106, 219, 157) 0%, rgb(23, 167, 88) 100%)";
+};
 
-const Premium = function(){
-    document.querySelector(".notification").style.display = "flex";
-    window.scrollBy(0,280)
-    document.querySelector('#second').style.background = 'linear-gradient(135deg, rgb(106, 219, 157) 0%, rgb(23, 167, 88) 100%)';
-    
-}
+document.querySelector("#first").addEventListener("click", learnMore);
 
+document.body.addEventListener("click", Undo);
 
-document.querySelector("#first").addEventListener("click", learnMore)
-
-document.body.addEventListener('click',Undo);
-
-document.querySelector("#second").addEventListener('click', Premium)
-
-    
-
-
-
-
+document.querySelector("#second").addEventListener("click", Premium);
